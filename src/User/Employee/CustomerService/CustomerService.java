c06589e5706828ac8917344480d9b9d67bb986b4
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 01:22:27 +0600

    TODO implementations of CS previleges

--- src/User/Employee/CustomerService/CustomerService.java
+++ src/User/Employee/CustomerService/CustomerService.java
@@ -8,4 +8,34 @@ import User.Employee.Employee;
 
 public class CustomerService extends Employee implements CSPrivilages{
     ArrayList<Customer> customersUnderEmp;
+
+    @Override
+    public void viewCustomerPersonalisation() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewCustomerDetails() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void contactCustomer() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void contactDesigner() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void contactSupervisor() {
+        // TODO Auto-generated method stub
+        
+    }
 }
