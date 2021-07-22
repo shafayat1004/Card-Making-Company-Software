da8fc60961ae48d2da3650a503f1cad3973b2c8e
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 00:50:51 +0600

    placeholder functions for implementation later

--- src/User/Owner/Owner.java
+++ src/User/Owner/Owner.java
@@ -6,5 +6,53 @@ import Privilages.Supervisor.SupervisorPrivilages;
 import User.User;
 
 public class Owner extends User implements OwnerPrivilages, SupervisorPrivilages, CSPrivilages{
+
+    @Override
+    public void hireSupervisor() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void fireSupervisor() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void hireDesigner() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void fireDesigner() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void checkFinancials() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void paySalaryToSupervisor() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void paySalaryToSupervisorForCSEmployees() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void modifySalary() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
