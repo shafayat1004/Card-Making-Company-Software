269f623f49ef99e4ae619ff0e697697536ac3879
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 01:08:14 +0600

    Supervisor level TODO implementations added

--- src/User/Owner/Owner.java
+++ src/User/Owner/Owner.java
@@ -60,5 +60,53 @@ public class Owner extends User implements OwnerPrivilages, SupervisorPrivilages
         // TODO Auto-generated method stub
         
     }
+
+    @Override
+    public void paySalaryToCSEmployees() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void sendTransactionDetailsToServer() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void assignCSEmp2Cus() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void assignDes2Cus() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewComplaints() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewTransactionHistory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewCusAssigned2CSEmp() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewCusAssigned2Des() {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
