8bfbbd346c0081f65dd00d699bff4d99a46fb555
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 01:07:22 +0600

    Supervisor Privelages added

--- src/Privilages/Supervisor/SupervisorPrivilages.java
+++ src/Privilages/Supervisor/SupervisorPrivilages.java
@@ -1,5 +1,13 @@
 package Privilages.Supervisor;
 
 public interface SupervisorPrivilages {
-    
+    public void paySalaryToCSEmployees();
+    public void sendTransactionDetailsToServer();
+    public void assignCSEmp2Cus();
+    public void assignDes2Cus();
+    public void viewComplaints();
+    public void viewTransactionHistory();
+    public void viewCusAssigned2CSEmp();
+    public void viewCusAssigned2Des();
+
 }
