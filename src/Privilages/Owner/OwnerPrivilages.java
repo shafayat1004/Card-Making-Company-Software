aebd4b3050a4183ed1762d8eb04d2af89cccf4a8
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 01:21:45 +0600

    Removed setmeeting Contact without owner privelage can be done

--- src/Privilages/Owner/OwnerPrivilages.java
+++ src/Privilages/Owner/OwnerPrivilages.java
@@ -9,5 +9,4 @@ public interface OwnerPrivilages {
     public void paySalaryToSupervisor();
     public void paySalaryToSupervisorForCSEmployees();
     public void modifySalary();
-    public void setMeetingWithSupervisor();
 }
