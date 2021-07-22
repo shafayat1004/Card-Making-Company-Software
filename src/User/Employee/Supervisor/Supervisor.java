4684348d1adb5f930e9ac8c2ae10d42289ef6f4a
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 01:07:48 +0600

    Supervisor TODO implementations added

--- src/User/Employee/Supervisor/Supervisor.java
+++ src/User/Employee/Supervisor/Supervisor.java
@@ -9,4 +9,52 @@ import User.Employee.CustomerService.CustomerService;
 
 public class Supervisor extends Employee implements SupervisorPrivilages, CSPrivilages{
     ArrayList<CustomerService> CSEmpUnderSup;
+
+    @Override
+    public void paySalaryToCSEmployees() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void sendTransactionDetailsToServer() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void assignCSEmp2Cus() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void assignDes2Cus() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewComplaints() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewTransactionHistory() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewCusAssigned2CSEmp() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void viewCusAssigned2Des() {
+        // TODO Auto-generated method stub
+        
+    }
 }
