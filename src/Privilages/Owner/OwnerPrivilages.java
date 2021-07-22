3cd81443bea36e2d4bd789368750a7ab3d049a07
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 00:48:29 +0600

    Added Some privilages solely for Owner

--- src/Privilages/Owner/OwnerPrivilages.java
+++ src/Privilages/Owner/OwnerPrivilages.java
@@ -1,5 +1,12 @@
 package Privilages.Owner;
 
 public interface OwnerPrivilages {
-    
+    public void hireSupervisor();
+    public void fireSupervisor();
+    public void hireDesigner();
+    public void fireDesigner();
+    public void checkFinancials();
+    public void paySalaryToSupervisor();
+    public void paySalaryToSupervisorForCSEmployees();
+    public void modifySalary();
 }
