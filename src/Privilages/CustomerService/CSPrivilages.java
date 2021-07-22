5c23705104cc8bbb091cffe7ee0f13006b8b0211
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-23 01:20:15 +0600

    added customer services privileges

--- src/Privilages/CustomerService/CSPrivilages.java
+++ src/Privilages/CustomerService/CSPrivilages.java
@@ -1,5 +1,10 @@
 package Privilages.CustomerService;
 
 public interface CSPrivilages {
+    public void viewCustomerPersonalisation();
+    public void viewCustomerDetails();
+    public void contactCustomer();
+    public void contactDesigner();
+    public void contactSupervisor();
     
 }
