d87d03ebc5c8be4e769376f8bf2843903ad121f0
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-27 23:57:09 +0600

    changed app to main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,18 @@
 package application;
 import application.User.Employee.Supervisor.Supervisor;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
+public class Main extends Application{
     public static void main(String[] args) throws Exception {
         Supervisor e1 = new Supervisor();
         e1.setName("Ononto Jolil");
         System.out.println(e1);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        // TODO Auto-generated method stub
+        
     }
 }
