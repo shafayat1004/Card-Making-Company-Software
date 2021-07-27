65edd9bbb43706cfcd5d9cc05dcb47959dcc2f3e
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-28 01:56:12 +0600

    Added code to launch WelcomeScene on app start

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,6 +1,10 @@
 package application;
 
+import java.net.URL;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application{
@@ -9,8 +13,12 @@ public class Main extends Application{
     }
 
     @Override
-    public void start(Stage arg0) throws Exception {
-        // TODO Auto-generated method stub
-        
+    public void start(Stage stage) throws Exception {
+        URL welcomeScene = getClass().getResource("WelcomeScene.fxml");
+        Parent root = FXMLLoader.load(welcomeScene);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setTitle("CompanyName Operations");
+        stage.show();
     }
 }
