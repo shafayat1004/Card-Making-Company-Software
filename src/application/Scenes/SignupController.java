9f4315e6fbf031a8de268ee4d1fbf32b9536f4cc
Mir Shafayat Ahmed <shafayat1004@live.com>
2021-07-28 02:03:32 +0600

    Signup fxml and controller made added necessary text fields

---
+++ src/application/Scenes/SignupController.java
@@ -0,0 +1,77 @@
+package application.Scenes;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class SignupController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private TextField confirmPassTextField;
+
+    @FXML
+    private Button signupCheckButton;
+
+    @FXML
+    private Button signinButton;
+
+    @FXML
+    private TextField idTextField;
+
+    @FXML
+    private TextField passwordTextField;
+
+    private Parent root;
+    private Scene scene;
+    private Stage stage;
+
+    @FXML
+    void signinButtonOnClick(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("Signin.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void backToWelComeOnClick(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("..\\WelcomeScene.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    void signupCheckButtonOnClick(ActionEvent event) {
+
+    }
+
+    @FXML
+    void initialize() {
+        assert emailTextField != null : "fx:id=\"emailTextField\" was not injected: check your FXML file 'Signup.fxml'.";
+        assert confirmPassTextField != null : "fx:id=\"confirmPassTextField\" was not injected: check your FXML file 'Signup.fxml'.";
+        assert signupCheckButton != null : "fx:id=\"signupCheckButton\" was not injected: check your FXML file 'Signup.fxml'.";
+        assert signinButton != null : "fx:id=\"signinButton\" was not injected: check your FXML file 'Signup.fxml'.";
+        assert idTextField != null : "fx:id=\"idTextField\" was not injected: check your FXML file 'Signup.fxml'.";
+        assert passwordTextField != null : "fx:id=\"passwordTextField\" was not injected: check your FXML file 'Signup.fxml'.";
+
+    }
+}
