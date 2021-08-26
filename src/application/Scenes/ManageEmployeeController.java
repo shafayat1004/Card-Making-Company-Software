3a0dc46adc04a2e111cfc1bf7396ca1b62c66b68
Shafayat1004 <shafayat1004@live.com>
2021-08-26 19:53:50 +0600

    Employee management functionality. Scene and Controller added for owner dashboard.

---
+++ src/application/Scenes/ManageEmployeeController.java
@@ -0,0 +1,19 @@
+package application.Scenes;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+
+public class ManageEmployeeController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    void initialize() {
+
+    }
+}
