5777bef7d44478503bee08f1e0a0f28441c1deda
Shafayat1004 <shafayat1004@live.com>
2021-08-26 19:49:25 +0600

    Modified Address fields

--- src/application/Database/Address/Address.java
+++ src/application/Database/Address/Address.java
@@ -1,108 +1,69 @@
 package application.Database.Address;
 
 public class Address {
-    String houseName, streetName, block, area, thana, zipcode, city, district, upazila, division;
+    
+    private String userID;
+    private String floorNo, houseDetails, streetDetails, blockAndArea, district, upazillaOrCCorp, division;
 
-
-    @Override
-    public String toString() {
-        return "Address [houseName=" + houseName + ", streetName=" + streetName + ", block=" + block + ", area=" + area + ", thana="
-                + thana + ", upazila=" + upazila + ", zipcode=" + zipcode + ", city=" + city + ", district=" + district+ ", division=" + division
-                + "]";
-    }
-
-    public Address(String houseName, String streetName, String block, String area, String thana, String zipcode,
-            String city, String district, String upazila, String division) {
-        this.houseName = houseName;
-        this.streetName = streetName;
-        this.block = block;
-        this.area = area;
-        this.thana = thana;
-        this.zipcode = zipcode;
-        this.city = city;
+    public Address(String userID, String floorNo, String houseDetails, String streetDetails, String blockAndArea,
+            String district, String upazillaOrCCorp, String division) {
+        this.userID = userID;
+        this.floorNo = floorNo;
+        this.houseDetails = houseDetails;
+        this.streetDetails = streetDetails;
+        this.blockAndArea = blockAndArea;
         this.district = district;
-        this.upazila = upazila;
+        this.upazillaOrCCorp = upazillaOrCCorp;
         this.division = division;
     }
-    
+
+    public String getUserID() {
+        return userID;
+    }
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+    public String getFloorNo() {
+        return floorNo;
+    }
+    public void setFloorNo(String floorNo) {
+        this.floorNo = floorNo;
+    }
+    public String getHouseDetails() {
+        return houseDetails;
+    }
+    public void setHouseDetails(String houseDetails) {
+        this.houseDetails = houseDetails;
+    }
+    public String getStreetDetails() {
+        return streetDetails;
+    }
+    public void setStreetDetails(String streetDetails) {
+        this.streetDetails = streetDetails;
+    }
+    public String getBlockAndArea() {
+        return blockAndArea;
+    }
+    public void setBlockAndArea(String blockAndArea) {
+        this.blockAndArea = blockAndArea;
+    }
+    public String getDistrict() {
+        return district;
+    }
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+    public String getUpazillaOrCCorp() {
+        return upazillaOrCCorp;
+    }
+    public void setUpazillaOrCCorp(String upazillaOrCCorp) {
+        this.upazillaOrCCorp = upazillaOrCCorp;
+    }
     public String getDivision() {
         return division;
     }
-
     public void setDivision(String division) {
         this.division = division;
     }
 
-    public String getDistrict() {
-        return district;
-    }
-
-    public void setDistrict(String district) {
-        this.district = district;
-    }
-
-    public String getUpazila() {
-        return upazila;
-    }
-
-    public void setUpazila(String upazila) {
-        this.upazila = upazila;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getZipcode() {
-        return zipcode;
-    }
-
-    public void setZipcode(String zipcode) {
-        this.zipcode = zipcode;
-    }
-
-    public String getThana() {
-        return thana;
-    }
-
-    public void setThana(String thana) {
-        this.thana = thana;
-    }
-
-    public String getArea() {
-        return area;
-    }
-
-    public void setArea(String area) {
-        this.area = area;
-    }
-
-    public String getBlock() {
-        return block;
-    }
-
-    public void setBlock(String block) {
-        this.block = block;
-    }
-
-    public String getStreetName() {
-        return streetName;
-    }
-
-    public void setStreetName(String streetName) {
-        this.streetName = streetName;
-    }
-
-    public String getHouseName() {
-        return houseName;
-    }
-
-    public void setHouseName(String houseName) {
-        this.houseName = houseName;
-    }
-    
 }
