d72c91387ff3a2e4898e7a1b4fec3675e501398a
Shafayat1004 <shafayat1004@live.com>
2021-08-28 02:38:01 +0600

    hire functions implemented.

--- src/application/User/Owner/Owner.java
+++ src/application/User/Owner/Owner.java
@@ -6,6 +6,8 @@ import application.Database.DatabaseManipulator;
 import application.Privilages.CustomerService.CSPrivilages;
 import application.Privilages.Supervisor.SupervisorPrivilages;
 import application.User.User;
+import application.User.Employee.CustomerService.CustomerService;
+import application.User.Employee.Designer.Designer;
 import application.User.Employee.Supervisor.Supervisor;
 import javafx.scene.image.Image;
 
@@ -21,11 +23,39 @@ public class Owner extends User implements SupervisorPrivilages, CSPrivilages{
     }
 
 
+    public String[] hire (String designation, String name, String mobileNum, String nationalID, String email, Image dp){
+        if(designation == Assets.getUserTypes()[1]){ //customer service
+            return hireCustomerService(name, mobileNum, nationalID, email, dp);
+        }
+        else if(designation == Assets.getUserTypes()[2]){ //supervisor
+            return hireSupervisor(name, mobileNum, nationalID, email, dp);
+
+        }
+        else if(designation == Assets.getUserTypes()[3]){ //designer
+            return hireDesigner(name, mobileNum, nationalID, email, dp);
+        }
+        else{
+            return new String[]{"UNSUCCESFULL", "UNSUCCESFULL"};
+        }
+    }
+
+    private String[] hireCustomerService(String name, String mobileNum, String nationalID, String email, Image dp) {
+
+        String genPass = ((Integer)(int)Math.floor(Math.random()*(9999999-1000000+1)+1000000)).toString();
+        String genID = "CSE" + ((Integer)(DatabaseManipulator.getCount(Assets.getcSEmployeesFilePath()) + 1)).toString();
+
+        CustomerService newCustomerService = new CustomerService(name, mobileNum, genID, nationalID, email, genPass, dp);
+        DatabaseManipulator.writeToDatabase(Assets.getSupervisorFilePath(), newCustomerService, true);
+        Credentials credentials = new Credentials(email, genID, genPass, Assets.getUserTypes()[1]);
+        DatabaseManipulator.writeToDatabase(Assets.getCredentialsFilePath(), credentials, true);
+
+        return new String[]{genID, genPass};
+    }
 
     public String[] hireSupervisor(String name, String mobileNum, String nationalID, String email, Image dp) {
         
         String genPass = ((Integer)(int)Math.floor(Math.random()*(9999999-1000000+1)+1000000)).toString();
-        String genID = ((Integer)(DatabaseManipulator.getCount(Assets.getSupervisorFilePath()) + 1)).toString();
+        String genID = "SUP" + ((Integer)(DatabaseManipulator.getCount(Assets.getSupervisorFilePath()) + 1)).toString();
 
         Supervisor newSupervisor = new Supervisor(name, mobileNum, genID, nationalID, email, genPass, dp);
         DatabaseManipulator.writeToDatabase(Assets.getSupervisorFilePath(), newSupervisor, true);
@@ -36,6 +66,18 @@ public class Owner extends User implements SupervisorPrivilages, CSPrivilages{
         
     }
 
+    public String[] hireDesigner(String name, String mobileNum, String nationalID, String email, Image dp) {
+        String genPass = ((Integer)(int)Math.floor(Math.random()*(9999999-1000000+1)+1000000)).toString();
+        String genID = "DES" + ((Integer)(DatabaseManipulator.getCount(Assets.getDesignersFilePath()) + 1)).toString();
+
+        Designer newDesigner = new Designer(name, mobileNum, genID, nationalID, email, genPass, dp);
+        DatabaseManipulator.writeToDatabase(Assets.getSupervisorFilePath(), newDesigner, true);
+        Credentials credentials = new Credentials(email, genID, genPass, Assets.getUserTypes()[3]);
+        DatabaseManipulator.writeToDatabase(Assets.getCredentialsFilePath(), credentials, true);
+
+        return new String[]{genID, genPass};
+    }
+
 
     public void fireSupervisor() {
         // TODO Auto-generated method stub
@@ -43,10 +85,6 @@ public class Owner extends User implements SupervisorPrivilages, CSPrivilages{
     }
 
 
-    public void hireDesigner() {
-        // TODO Auto-generated method stub
-        
-    }
 
 
     public void fireDesigner() {
