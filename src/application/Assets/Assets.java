5f7e8bf47896141ff61fabf479a83fede32c6d6f
Shafayat1004 <shafayat1004@live.com>
2021-08-31 05:30:05 +0600

    Changed all fields to public for ease simplicity. ALl other files depening on it now access the fields directly

--- src/application/Assets/Assets.java
+++ src/application/Assets/Assets.java
@@ -1,51 +1,52 @@
 package application.Assets;
 
 public abstract class Assets {
-    private static String credentialsFilePath = "src/application/Database/Credentials.bin";
-    private static String customersFilePath = "src/application/Database/Customers.bin";
-    private static String cSEmployeesFilePath = "src/application/Database/EmployeeList/CSEmployees.bin";
-    private static String supervisorFilePath = "src/application/Database/EmployeeList/Supervisors.bin";
-    private static String designersFilePath = "src/application/Database/EmployeeList/Designers.bin";
-    private static String ownersFilePath = "src/application/Database/Owners.bin";
-    private static String[] userTypes = {"Customer", "Customer Service Employee", "Supervisor", "Designer", "Owner"};
-    private static String[] districts = {"Dhaka", "Chittagong", "Khulna", "Rajshahi", "Sylhet"}; //TODO not all districts added
-    private static String addressesFilePath = "src/application/Database/Addresses.bin";
-    private static String currentUserFilePath = "src/application/Database/CurrentUser.bin";
-    private static String defaultProfilePicture = "src/application/Assets/EmptyPicture.png";
+    public static String credentialsFilePath = "src/application/Database/Credentials.bin";
+    public static String customersFilePath = "src/application/Database/Customers.bin";
+    public static String cSEmployeesFilePath = "src/application/Database/EmployeeList/CSEmployees.bin";
+    public static String supervisorFilePath = "src/application/Database/EmployeeList/Supervisors.bin";
+    public static String designersFilePath = "src/application/Database/EmployeeList/Designers.bin";
+    public static String ownersFilePath = "src/application/Database/Owners.bin";
+    public static String[] userTypes = {"Customer", "Customer Service Employee", "Supervisor", "Designer", "Owner"};
+    public static String[] districts = {"Dhaka", "Chittagong", "Khulna", "Rajshahi", "Sylhet"}; //TODO not all districts added
+    public static String addressesFilePath = "src/application/Database/Addresses.bin";
+    public static String currentUserFilePath = "src/application/Database/CurrentUser.bin";
+    public static String defaultProfilePicture = "src/application/Assets/EmptyPicture.png";
+    public static String imageSavePath = "src/application/Database/Images/";
     
     
-    public static String getDefaultProfilePicture() {
-        return defaultProfilePicture;
-    }
-    public static String getCurrentUserFilePath() {
-        return currentUserFilePath;
-    }
-    public static String getCredentialsFilePath() {
-        return credentialsFilePath;
-    }
-    public static String getAddressesFilePath() {
-        return addressesFilePath;
-    }
-    public static String getCustomersFilePath() {
-        return customersFilePath;
-    }
-    public static String getcSEmployeesFilePath() {
-        return cSEmployeesFilePath;
-    }
-    public static String getSupervisorFilePath() {
-        return supervisorFilePath;
-    }
-    public static String getDesignersFilePath() {
-        return designersFilePath;
-    }
-    public static String getOwnersFilePath() {
-        return ownersFilePath;
-    }
-    public static String[] getUserTypes() {
-        return userTypes;
-    }
-    public static String[] getDistricts() {
-        return districts;
-    }
+    // public static String getImageSavePath() {
+    //     return imageSavePath;
+    // }
+    // public static String getDefaultProfilePicture() {
+    //     return defaultProfilePicture;
+    // }
+    // public static String getCurrentUserFilePath() {
+    //     return currentUserFilePath;
+    // }
+    // public static String getCredentialsFilePath() {
+    //     return credentialsFilePath;
+    // }
+    // public static String getAddressesFilePath() {
+    //     return addressesFilePath;
+    // }
+    // public static String getCustomersFilePath() {
+    //     return customersFilePath;
+    // }
+    // public static String getcSEmployeesFilePath() {
+    //     return cSEmployeesFilePath;
+    // }
+    // public static String getSupervisorFilePath() {
+    //     return supervisorFilePath;
+    // }
+    // public static String getDesignersFilePath() {
+    //     return designersFilePath;
+    // }
+    // public static String getOwnersFilePath() {
+    //     return ownersFilePath;
+    // }
+    // public static String[] getDistricts() {
+    //     return districts;
+    // }
     
 }
