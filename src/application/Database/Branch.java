01ea62202201de62daf97df4fdbea333358e6c2f
Shafayat1004 <shafayat1004@live.com>
2021-09-08 01:01:34 +0600

    added constructor and methods

--- src/application/Database/Branch.java
+++ src/application/Database/Branch.java
@@ -1,17 +1,33 @@
 package application.Database;
 
 import java.io.Serializable;
-import java.time.LocalDate;
+// import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Branch implements Serializable{
-    private String supervisorID, upazillaOrCCorp; //not scalabale as it assumes only one branch per upazillaorCityCorp
+    
+    private String upazillaOrCCorp; //Primary Field //not scalabale as it assumes only one branch per upazillaorCityCorp
+    private String supervisorID; 
     private ArrayList<String> customerServiceIDs;
     private int[] customersServedPerMonth;
-    private ArrayList<Integer> expensesPerMonth;
-    private ArrayList<Integer> revenuePerMonth;
+    private int[] expensesPerMonth;
+    private int[] revenuePerMonth;
 
+    public Branch(String upazillaOrCCorp){
+        this.upazillaOrCCorp = upazillaOrCCorp;
+        customerServiceIDs = new ArrayList<String>();
+        customersServedPerMonth = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+        expensesPerMonth = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+        revenuePerMonth = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+    }
+    public Branch(String supervisorID, ArrayList<String> customerServiceIDs){
+        this.supervisorID = supervisorID;
+        this.customerServiceIDs = customerServiceIDs;
+        customersServedPerMonth = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+        expensesPerMonth = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
+        revenuePerMonth = new int[]{0,0,0,0,0,0,0,0,0,0,0,0};
 
+    }
     public String getSupervisorID() {
         return supervisorID;
     }
@@ -33,11 +49,38 @@ public class Branch implements Serializable{
     public void setCustomerServiceIDs(ArrayList<String> customerServiceIDs) {
         this.customerServiceIDs = customerServiceIDs;
     }
-    public int getCurrentMonthCustomerServed() {
-        LocalDate currDate = LocalDate.now();
-        int currMonth = currDate.getMonthValue();
-        return customersServedPerMonth[currMonth-1];
-    }   
-    
+    // public int getCurrentMonthCustomerServed() {
+    //     LocalDate currDate = LocalDate.now();
+    //     int currMonth = currDate.getMonthValue();
+    //     return customersServedPerMonth[currMonth-1];
+    // }   
+    // public int getCurrentMonthExpenses() {
+    //     LocalDate currDate = LocalDate.now();
+    //     int currMonth = currDate.getMonthValue();
+    //     return expensesPerMonth[currMonth-1];
+    // }
+    // public int getCurrentMonthRevenue(){
+    //     LocalDate currDate = LocalDate.now();
+    //     int currMonth = currDate.getMonthValue();
+    //     return revenuePerMonth[currMonth-1];
+    // }
+    public int getExpensesOfMonth(int monthNo) {
+        return expensesPerMonth[monthNo-1];
+    }
+    public int getRevenueOfMonth(int monthNo) {
+        return revenuePerMonth[monthNo-1];
+    }
+    public int getCustomersServedOfMonth(int monthNo) {
+        return customersServedPerMonth[monthNo-1];
+    }
+    public void addExpensesOfMonth(int monthNo, int addedExpense) {
+        expensesPerMonth[monthNo-1] += addedExpense;
+    }
+    public void addRevenueOfMonth(int monthNo, int addedRevenue) {
+        revenuePerMonth[monthNo-1] += addedRevenue;
+    }
+    public void addCustomersServedOfMonth(int monthNo, int addedCustomers) {
+        customersServedPerMonth[monthNo-1] += addedCustomers;
+    }
 
 }
