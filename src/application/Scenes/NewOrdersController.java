c1e78b521c6cbad67e5a648245a786623644ff74
Shafayat1004 <shafayat1004@live.com>
2021-09-08 13:09:10 +0600

    can load orders and refresh now

--- src/application/Scenes/NewOrdersController.java
+++ src/application/Scenes/NewOrdersController.java
@@ -1,9 +1,13 @@
 package application.Scenes;
 
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ResourceBundle;
 
+import application.Database.DatabaseManipulator;
 import application.Order.Order;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
@@ -11,36 +15,52 @@ import javafx.scene.control.CheckBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextArea;
-import javafx.scene.layout.VBox;    
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.VBox;
 
 public class NewOrdersController extends Controller{
 
     @FXML private ResourceBundle resources;
     @FXML private URL location;
-
     @FXML private VBox orderActionsVBox;
-    @FXML private TableView<Order> newOrderTableView;
-    @FXML private TableColumn<Order, String> orderIDCol;
-    @FXML private TableColumn<Order, String> dateCol;
-    @FXML private TableColumn<Order, String> timeCol;
-    @FXML private TableColumn<Order, String> locationCol;
-    @FXML private TableColumn<Order, String> nameCol;
-    @FXML private TableColumn<Order, String> customerIDCol;
-    @FXML private TableColumn<Order, String> orderTypeCol;
-    @FXML private TableColumn<Order, String> orderQuantityCol;
-    @FXML private TableColumn<Order, String> deliverByCol;
     @FXML private TextArea customerNoteTextArea;
-    @FXML private TextArea rejectCauseTextArea;
     @FXML private Button viewDetailsButton;
     @FXML private Button rejectButton;
     @FXML private CheckBox giveCauseChkBox;
+    @FXML private TextArea rejectCauseTextArea;
     @FXML private Button assignCSEmpButton;
     @FXML private CheckBox csEmpChkBox;
     @FXML private Button assignDesignerButton;
     @FXML private CheckBox designerChkBox;
     @FXML private Button acceptButton;
+    @FXML private Button refreshButton;
 
+    @FXML private TableView<Order> newOrderTableView;
+    @FXML private TableColumn<Order, String> orderIDCol;
+    @FXML private TableColumn<Order, LocalDateTime> orderedOnCol;
+    @FXML private TableColumn<Order, String> branchCol;
+    @FXML private TableColumn<Order, String> customerIDCol;
+    @FXML private TableColumn<Order, String> orderTypeCol;
+    @FXML private TableColumn<Order, Integer> orderQuantityCol;
+    @FXML private TableColumn<Order, String> fileNameCol;
+    @FXML private TableColumn<Order, LocalDate> deliverByCol;
 
+    @FXML
+    void refreshButtonOnClick(ActionEvent event) {
+
+        ObservableList<Order> orderList = DatabaseManipulator.getOrderListFromDatabase();
+        orderIDCol.setCellValueFactory(new PropertyValueFactory<Order, String>("orderID"));
+        orderedOnCol.setCellValueFactory(new PropertyValueFactory<Order, LocalDateTime>("orderedOn"));
+        branchCol.setCellValueFactory(new PropertyValueFactory<Order, String>("deliverTo"));
+        customerIDCol.setCellValueFactory(new PropertyValueFactory<Order, String>("customerID"));
+        orderTypeCol.setCellValueFactory(new PropertyValueFactory<Order, String>("orderType"));
+        orderQuantityCol.setCellValueFactory(new PropertyValueFactory<Order, Integer>("orderQuantity"));
+        fileNameCol.setCellValueFactory(new PropertyValueFactory<Order, String>("fileName"));
+        deliverByCol.setCellValueFactory(new PropertyValueFactory<Order, LocalDate>("deliverBy"));
+
+        newOrderTableView.setItems(orderList);
+    }
+    
     @FXML
     void acceptButtonOnClick(ActionEvent event) {
         if (csEmpChkBox.isSelected() && designerChkBox.isSelected()) {
@@ -81,6 +101,8 @@ public class NewOrdersController extends Controller{
 
     @FXML
     void initialize() {
+        
+
         assert orderActionsVBox != null : "fx:id=\"orderActionsVBox\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert customerNoteTextArea != null : "fx:id=\"customerNoteTextArea\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert viewDetailsButton != null : "fx:id=\"viewDetailsButton\" was not injected: check your FXML file 'NewOrders.fxml'.";
@@ -94,13 +116,12 @@ public class NewOrdersController extends Controller{
         assert acceptButton != null : "fx:id=\"acceptButton\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert newOrderTableView != null : "fx:id=\"newOrderTableView\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert orderIDCol != null : "fx:id=\"orderIDCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
-        assert dateCol != null : "fx:id=\"dateCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
-        assert timeCol != null : "fx:id=\"timeCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
-        assert locationCol != null : "fx:id=\"locationCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
-        assert nameCol != null : "fx:id=\"nameCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
+        assert orderedOnCol != null : "fx:id=\"orderedOnCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
+        assert branchCol != null : "fx:id=\"branchCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert customerIDCol != null : "fx:id=\"customerIDCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert orderTypeCol != null : "fx:id=\"orderTypeCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert orderQuantityCol != null : "fx:id=\"orderQuantityCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
+        assert fileNameCol != null : "fx:id=\"fileNameCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
         assert deliverByCol != null : "fx:id=\"deliverByCol\" was not injected: check your FXML file 'NewOrders.fxml'.";
 
     }
