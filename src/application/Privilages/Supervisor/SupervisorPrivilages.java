9811a7a6f96da37fa7a484a8ea0e4107510a1fcc
Shafayat1004 <shafayat1004@live.com>
2021-09-08 13:06:26 +0600

    View new orders method created

--- src/application/Privilages/Supervisor/SupervisorPrivilages.java
+++ src/application/Privilages/Supervisor/SupervisorPrivilages.java
@@ -1,8 +1,10 @@
 package application.Privilages.Supervisor;
 
 public interface SupervisorPrivilages {
+
     public void paySalaryToCSEmployees();
     public void sendTransactionDetailsToServer();
+    public void viewNewOrders();
     public void assignCSEmp2Cus();
     public void assignDes2Cus();
     public void viewComplaints();
